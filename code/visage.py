af15104d9f2fe7a27bda689d2812e588d6791972
Nicolas Sénécal <35195832+NicolasSenecal@users.noreply.github.com>
2019-01-15 16:44:44 +0100

    Added visage class

---
+++ code/visage.py
@@ -0,0 +1,15 @@
+# visage.py
+# Visage class and functions
+
+# class that manages the information of a face on a given frame
+class Visage():
+  def __init__(self, confidence, x_min, x_max, y_min, y_max, yaw, pitch, roll):
+    self.confidence = confidence
+    self.x_min = x_min
+    self.x_max = x_max
+    self.y_min = y_min
+    self.y_max = y_max
+    self.yaw = yaw
+    self.pitch = pitch
+    self.roll = roll
+
